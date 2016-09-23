53e7256b61655db3f05ca66d15c08438aa637f0f
Mohit Agarwal <mohit@zenify.in>
2016-09-23 19:29:29 +0530

    Logging order creation event in filesystem

---
+++ app/code/local/InfusionsoftIntegration/OrderCreationEvent/Model/Observer.php
@@ -0,0 +1,31 @@
+
+<?php
+/**
+ * Our class name should follow the directory structure of
+ * our Observer.php model, starting from the namespace,
+ * replacing directory separators with underscores.
+ * i.e. app/code/local/InfusionsoftIntegration/OrderCreationEvent/Model/Observer.php
+ */
+class InfusionsoftIntegration_OrderCreationEvent_Model_Observer
+{
+    /**
+     * Magento passes a Varien_Event_Observer object as
+     * the first parameter of dispatched events.
+     */
+    public function logUpdate(Varien_Event_Observer $observer)
+    {
+        Mage::log("Got event of new order creation");
+
+        // Retrieve the order being created from the event observer
+        $order = $observer->getEvent()->getOrder();
+
+        // Write a new line to var/log/product-updates.log
+        $name = print_r($order, 1);
+
+        Mage::log(
+            "{$name} is the order created",
+            null,
+            'product-updates.log'
+        );
+    }
+}
