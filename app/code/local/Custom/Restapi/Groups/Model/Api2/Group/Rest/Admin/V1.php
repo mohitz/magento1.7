f4b3f729e7bfa8a275d3ffe599069ab446ae3f91
Mohit Agarwal <mohit@zenify.in>
2016-09-24 18:45:53 +0530

    Implemented dummy REST API for customer groups.

--- app/code/local/Custom/Restapi/Groups/Model/Api2/Group/Rest/Admin/V1.php
+++ app/code/local/Custom/Restapi/Groups/Model/Api2/Group/Rest/Admin/V1.php
@@ -13,69 +13,67 @@ class Custom_Restapi_Groups_Model_Api2_Group_Rest_Admin_V1 extends Mage_Api2_Mod
         $incrementId = $this->getRequest()->getParam('id');
         $order = Mage::getModel('sales/order')->loadByIncrementId($incrementId);
 
-        return $order->getAllVisibleItems()[0]->getSku();
+        $orderItems = $order->getItemsCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('product_type', array('eq'=>'simple'))
+            ->load();
 
 
-//        $orderItems = $order->getItemsCollection()
-//            ->addAttributeToSelect('*')
-//            ->addAttributeToFilter('product_type', array('eq'=>'simple'))
-//            ->load();
-//
-//        $categoryArr = array();
-//        $superCategoryArr = array();
-//
-//        foreach($orderItems as $sItem) {
-//
-//            //Ignore conf for now
-//            //Alt. Mage_Catalog_Model_Product_Type::TYPE_SIMPLE = 'simple';
-//            if($sItem->getProductType() == "simple")
-//            {
-//
-//                $productId = $sItem->getProductId();
-//                $sku = $sItem->getSku();
-//
-//                $parentItemId = $sItem->getParentItemId();
-//
-//                //Get Parent Item Information
-//                $item = Mage::getModel('sales/order_item')->load("$parentItemId"); //use an item_id here
-//
-//                $productType = $item->getProductType();
-//                $parentOrderId = $item->getOrderId();
-//                $parentProductId = $item->getProductId();
-//                $itemId = $item->getId();
-//                $parentItemPrice = $item->getPrice();
-//                $quantity = $item->getQtyOrdered();
-//
-//                //get Active Product Data
-//                $nProduct = Mage::getModel('catalog/product')->load($sItem->getProductId());
-//                $nSku = $nProduct->getSku();
-//                $nUpc = $nProduct->getUpc();
-//                $nPrice = $nProduct->getPrice();
-//
-//                //Fetch category name
-//                $product = Mage::getModel('catalog/product')->load($item->getProductId());
-//                $categoryIds = $product->getCategoryIds();
-//
-//
-//                foreach ($categoryIds as $category_id) {
-//                    $category = Mage::getModel('catalog/category')->setStoreId(Mage::app()->getStore()->getId())->load($category_id);
-//
-//                    $categoryName = $category->getName();
-//                    $categoryArr.array_push($categoryName);
-//
-//                    $parentCategoryName = $category->getParentCategory()->getName();
-//                    $superCategoryArr = array_push($parentCategoryName);
-//
-//                }
-//
-//
-//            }
-//        }
-//
-//        $categoryString = implode(",", $categoryArr);
-//        $superCategoryString = implode(",", $superCategoryArr);
-//
-//        return $categoryString.$superCategoryString;
+        $categoryArr = array();
+        $superCategoryArr = array();
+
+        foreach($orderItems as $sItem) {
+
+            //Ignore conf for now
+            //Alt. Mage_Catalog_Model_Product_Type::TYPE_SIMPLE = 'simple';
+            if($sItem->getProductType() == "simple")
+            {
+
+                $productId = $sItem->getProductId();
+                $sku = $sItem->getSku();
+
+                $parentItemId = $sItem->getParentItemId();
+
+                //Get Parent Item Information
+                $item = Mage::getModel('sales/order_item')->load("$parentItemId"); //use an item_id here
+
+                $productType = $item->getProductType();
+                $parentOrderId = $item->getOrderId();
+                $parentProductId = $item->getProductId();
+                $itemId = $item->getId();
+                $parentItemPrice = $item->getPrice();
+                $quantity = $item->getQtyOrdered();
+
+                //get Active Product Data
+                $nProduct = Mage::getModel('catalog/product')->load($sItem->getProductId());
+                $nSku = $nProduct->getSku();
+                $nUpc = $nProduct->getUpc();
+                $nPrice = $nProduct->getPrice();
+
+                //Fetch category name
+                $product = Mage::getModel('catalog/product')->load($item->getProductId());
+                $categoryIds = $product->getCategoryIds();
+
+
+                foreach ($categoryIds as $category_id) {
+                    $category = Mage::getModel('catalog/category')->setStoreId(Mage::app()->getStore()->getId())->load($category_id);
+
+                    $categoryName = $category->getName();
+                    array_push($categoryArr, $categoryName);
+
+                    $parentCategoryName = $category->getParentCategory()->getName();
+                    array_push($superCategoryArr, $parentCategoryName);
+
+                }
+
+
+            }
+        }
+
+        $categoryString = implode(",", $categoryArr);
+        $superCategoryString = implode(",", $superCategoryArr);
+
+        return $categoryString.$superCategoryString;
 
     }
 
